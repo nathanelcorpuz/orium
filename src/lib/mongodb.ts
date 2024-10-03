9db7876f105be5c125114bf09228a09dd49685e6
natdlc <nathanelcorpuz@gmail.com>
2024-10-03 19:21:36 +0800

    update

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 const { MONGODB_URI } = process.env;
+
+const env = process.env.NODE_ENV;
+
+const URI =
+	env === "production" ? MONGODB_URI : "mongodb://127.0.0.1:27017/orium";
+
 export const connectDB = async () => {
 	try {
-		const { connection } = await mongoose.connect(MONGODB_URI as string);
+		const { connection } = await mongoose.connect(URI as string);
 		if (connection.readyState === 1) {
 			return Promise.resolve(true);
 		}

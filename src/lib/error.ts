367b342cc95ceab1699dde0d137c06343723bf36
natdlc <nathanelcorpuz@gmail.com>
2024-10-08 23:09:10 +0800

    update

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -3,7 +3,7 @@ export const errorHandler = (error: Error) => {
 	console.log("ERROR!!");
 	console.log(error);
 	console.log("\n\n");
-	return new Response(JSON.stringify(error.message), {
+	return new Response(JSON.stringify({ message: error.message }), {
 		status: 500,
 		statusText: error.message,
 	});

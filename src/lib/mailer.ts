243e1ca18d1d6ea82990019f124a341dad3b8db6
natdlc <nathanelcorpuz@gmail.com>
2024-10-08 23:24:16 +0800

    update

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,11 +1,20 @@
 import nodemailer from "nodemailer";
 
+// export const transporter = nodemailer.createTransport({
+// 	host: process.env.MAILER_HOST,
+// 	port: Number(process.env.MAILER_PORT),
+// 	secure: true,
+// 	auth: {
+// 		user: process.env.MAILER_EMAIL,
+// 		pass: process.env.MAILER_PASS,
+// 	},
+// });
 export const transporter = nodemailer.createTransport({
-	host: process.env.MAILER_HOST,
-	port: Number(process.env.MAILER_PORT),
+	host: "smtp.ethereal.email",
+	port: 587,
 	secure: true,
 	auth: {
-		user: process.env.MAILER_EMAIL,
-		pass: process.env.MAILER_PASS,
+		user: "kellie.reichel@ethereal.email",
+		pass: "2bEAbP6yNunAkuckm8",
 	},
 });
